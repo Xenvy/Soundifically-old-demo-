c0c97b38e53c749d690492eff24d5f82f9136600
SHDW411 <38317328+SHDW411@users.noreply.github.com>
2020-05-26 23:48:32 +0200

    Add files via upload

--- Panorama_2.0/Assets/Click_reste_button.cs
+++ Panorama_2.0/Assets/Click_reste_button.cs
@@ -5,41 +5,47 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 
+
 public class Click_reste_button : MonoBehaviour
 {
     public GameObject circelPrefab;
-
-    public GameObject MamasAndPapas;
 
     public Vector3 center;
     public Vector3 size;
 
     public int life = 100;
     public int score = 0;
+    public int level = 1;
 
+    public Text scoreText;
+    public Text lifeText;
+    public Text levelText;
 
+    public Texture2D gameCoursor;
+
+    public int sound_r = 0;
+    public int sound_x = 0;
+
+    //FMOD instance initiation
     [FMODUnity.EventRef]
     public string bul_sound = "event:/bul";
     FMOD.Studio.EventInstance bul_sound_instance;
-
-    public float health = 0;
-
     Rigidbody cachedRigidBody;
 
     void Start()
     {
+        //FMOD passing parameters
         bul_sound_instance = FMODUnity.RuntimeManager.CreateInstance(bul_sound);
-
         bul_sound_instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject, cachedRigidBody));
-        bul_sound_instance.setParameterByName("Sound_set", health);
+        bul_sound_instance.setParameterByName("sound_r", sound_r);
+        bul_sound_instance.setParameterByName("sound_x", sound_x);
         bul_sound_instance.start();
     }
 
 
-    // Update is called once per frame
+
     void Update()
     {
-
         cachedRigidBody = GetComponent<Rigidbody>();
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
@@ -47,16 +53,16 @@ public class Click_reste_button : MonoBehaviour
 
         RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
 
- 
 
         if (hit.collider == null && Input.GetMouseButtonDown(0))
         {
             life = life - 10;
             Console.WriteLine(life);
+            lifeText.text = life.ToString();
         }
         else if (hit.collider != null)
         {
-            if(life > 0) 
+            if (life > 0)
             {
                 if (hit.collider)
                 {
@@ -66,42 +72,48 @@ public class Click_reste_button : MonoBehaviour
                         if (Input.GetMouseButtonDown(0))
                         {
                             bul_sound_instance.release();
-                            bul_sound_instance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
-                            score += 10;
-                            health += 20;
-                            Console.WriteLine(score);
-                            Console.WriteLine(health);
+                            bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                            score += level*life;
+                            scoreText.text = score.ToString();
+                            levelText.text = level.ToString();
                             SpawnNextButton();
                         }
                     }
                 }
             }
         }
-        
+        else
+        {
+
+        }
+
     }
 
     void SpawnNextButton()
     {
-        float xx = UnityEngine.Random.Range(-3.65f, 3.65f);
-        float yy = UnityEngine.Random.Range(-3.65f, 3.65f);
+        float x = UnityEngine.Random.Range(-3.65f, 3.65f);
+        Double y = Math.Sqrt(Math.Pow(3.65f, 2) - Math.Pow(x, 2));
+        float yf = Convert.ToSingle(y);
+        float yy = UnityEngine.Random.Range(-yf, yf);
         float zz = UnityEngine.Random.Range(1, 3);
 
-        Vector3 pos = center + new Vector3(xx, yy, 0);
-
-
-
-
-
-
+        Vector3 pos = center + new Vector3(x, yy, 0);
+        sound_r = Convert.ToInt32((Math.Sqrt(Math.Pow(yy, 2) + Math.Pow(x, 2))) * 10);
+        sound_x = Math.Abs(Convert.ToInt32((Math.Atan(yy / x)) * 10));
         Instantiate(circelPrefab, pos, Quaternion.identity);
 
         Destroy(gameObject);
-       
+
     }
 
-    void OnDestroy()
+    void GameOver()
     {
-       GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+
     }
 
+    void OnMouseEnter()
+    {
+        //Cursor.visible = false;
+        //Cursor.SetCursor(gameCoursor, Vector2.zero, CursorMode.ForceSoftware);
+    }
 }
