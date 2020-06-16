886438500ef5c32f6206ed42e5abfd3ff5f05e10
SHDW411 <38317328+SHDW411@users.noreply.github.com>
2020-06-16 22:34:31 +0200

    Add files via upload

--- Panorama_2.0/Assets/Click_reste_button.cs
+++ Panorama_2.0/Assets/Click_reste_button.cs
@@ -5,22 +5,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using UnityEngine.SceneManagement;
-
+using System.Data.SqlTypes;
+using System.CodeDom.Compiler;
+using UnityEditor;
 
 public class Click_reste_button : MonoBehaviour
 {
     public GameObject circelPrefab;
+    public GameObject GameOverMenu;
+    public GameObject WinningMenu;
+    public GameObject SpeakerPoint;
 
     public Vector3 center;
     public Vector3 size;
+    public Vector3 scaleChange;
+    public Vector3 speakrePos;
 
     public int life = 100;
     public int score = 0;
     public int level = 1;
-    public float vol = 50;
     public int marker = 1;
+    public int shot = 0;
 
     public Text scoreText;
+    public Text scoreText2;
+    public Text scoreText3;
     public Text lifeText;
     public Text levelText;
 
@@ -37,13 +46,13 @@ public class Click_reste_button : MonoBehaviour
 
     void Start()
     {
+
         //FMOD passing parameters
         bul_sound_instance = FMODUnity.RuntimeManager.CreateInstance(bul_sound);
         bul_sound_instance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject, cachedRigidBody));
         bul_sound_instance.setParameterByName("sound_r", sound_r);
         bul_sound_instance.setParameterByName("sound_x", sound_x);
         bul_sound_instance.setParameterByName("Track", marker);
-        bul_sound_instance.setVolume(5f);
         bul_sound_instance.start();
     }
 
@@ -64,44 +73,49 @@ public class Click_reste_button : MonoBehaviour
             life = life - 10;
             Console.WriteLine(life);
             lifeText.text = life.ToString();
-        }
-        else if (hit.collider != null)
-        {
-            if (life > 0)
-            {
-                if (hit.collider)
-                {
-
-                    if (hit.collider.tag == "Reset_button")
-                    {
-                        if (Input.GetMouseButtonDown(0))
-                        {
-                            bul_sound_instance.release();
-                            bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-                            score += level * life;
-                            scoreText.text = score.ToString();
-                            levelText.text = level.ToString();
-                            SpawnNextButton();
-                        }
-                    }
-                    else if (hit.collider.tag == "VolumeMenu" && Input.GetMouseButtonDown(0))
-                    {
-                        SetVolume(vol);
-                    }
-                    else if (hit.collider.tag == "Back" && Input.GetMouseButtonDown(0))
-                    {
-                        bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-                    }
-                }
-
-            }
-            else
+            if (life == 0)
             {
                 GameOver();
             }
         }
-        else
+        else if (hit.collider != null)
         {
+
+            if (hit.collider)
+            {
+
+                if (hit.collider.tag == "Reset_button")
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        bul_sound_instance.release();
+                        bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                        score += level * life;
+                        scoreText.text = score.ToString();
+                        scoreText2.text = score.ToString();
+                        scoreText3.text = score.ToString();
+                        levelText.text = level.ToString();
+                        shot += 1;
+                        if (shot % 5 == 0)
+                        {
+                            scaleChange = new Vector3(-1.8f, -1.8f , 0);
+                            circelPrefab.transform.localScale += scaleChange;
+                            level += 1;
+                            shot = 0;
+
+                        }
+                        if (level == 11)
+                        {
+                            Winning();
+                        }
+                        SpawnNextButton();
+                    }
+                }
+                else if (hit.collider.tag == "Back" && Input.GetMouseButtonDown(0))
+                {
+                    bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                }
+            }
 
         }
 
@@ -113,12 +127,16 @@ public class Click_reste_button : MonoBehaviour
         Double y = Math.Sqrt(Math.Pow(3.65f, 2) - Math.Pow(x, 2));
         float yf = Convert.ToSingle(y);
         float yy = UnityEngine.Random.Range(-yf, yf);
-        float zz = UnityEngine.Random.Range(1, 3);
-
+        int zz = UnityEngine.Random.Range(1, 11);
+ 
         Vector3 pos = center + new Vector3(x, yy, 0);
+        speakrePos = center + new Vector3(x, yy, 0);
+        SpeakerPoint.transform.localPosition = speakrePos;
+
         sound_r = Convert.ToInt32((Math.Sqrt(Math.Pow(yy, 2) + Math.Pow(x, 2))) * 10);
         sound_x = Convert.ToInt32((Math.Atan2(yy, x) * 10));
-        marker = Convert.ToInt32(zz);
+        marker = zz;
+
         Instantiate(circelPrefab, pos, Quaternion.identity);
 
         Destroy(gameObject);
@@ -127,35 +145,55 @@ public class Click_reste_button : MonoBehaviour
 
     void GameOver()
     {
-        cachedRigidBody = GetComponent<Rigidbody>();
-
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-
         bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-        if (hit.collider.tag == "VolumeMenu" && Input.GetMouseButtonDown(0))
+
+        if (score > PlayerPrefs.GetInt("PanHS1", 0))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            PlayerPrefs.SetInt("PanHS3", PlayerPrefs.GetInt("PanHS2", 0));
+            PlayerPrefs.SetInt("PanHS2", PlayerPrefs.GetInt("PanHS1", 0));
+            PlayerPrefs.SetInt("PanHS1", score);
         }
+        else if (score < PlayerPrefs.GetInt("PanHS1", 0) && score > PlayerPrefs.GetInt("PanHS2", 0))
+        {
+            PlayerPrefs.SetInt("PanHS3", PlayerPrefs.GetInt("PanHS2", 0));
+            PlayerPrefs.SetInt("PanHS2", score);
+        }
+        else if (score < PlayerPrefs.GetInt("PanHS2", 0) && score > PlayerPrefs.GetInt("PanHS3", 0))
+        {
+            PlayerPrefs.SetInt("PanHS3", score);
+        }
+
+        Destroy(gameObject);
+        GameOverMenu.SetActive(true);
+        SpeakerPoint.SetActive(true);
+
+
     }
 
-    public void MusicStop()
+    void Winning()
     {
+
+        if (score > PlayerPrefs.GetInt("PanHS1", 0))
+        {
+            PlayerPrefs.SetInt("PanHS3", PlayerPrefs.GetInt("PanHS2", 0));
+            PlayerPrefs.SetInt("PanHS2", PlayerPrefs.GetInt("PanHS1", 0));
+            PlayerPrefs.SetInt("PanHS1", score);
+        }
+        else if (score < PlayerPrefs.GetInt("PanHS1", 0) && score > PlayerPrefs.GetInt("PanHS2", 0))
+        {
+            PlayerPrefs.SetInt("PanHS3", PlayerPrefs.GetInt("PanHS2", 0));
+            PlayerPrefs.SetInt("PanHS2", score);
+        }
+        else if (score < PlayerPrefs.GetInt("PanHS2", 0) && score > PlayerPrefs.GetInt("PanHS3", 0))
+        {
+            PlayerPrefs.SetInt("PanHS3", score);
+        }
+
         bul_sound_instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+
+        WinningMenu.SetActive(true);
     }
 
-    public void SetVolume(float newVolume)
-    {
-        vol = newVolume;
-        bul_sound_instance.setVolume(vol);
-    }
+   
 
-
-    void OnMouseEnter()
-    {
-        //Cursor.visible = false;
-        //Cursor.SetCursor(gameCoursor, Vector2.zero, CursorMode.ForceSoftware);
-    }
 }
